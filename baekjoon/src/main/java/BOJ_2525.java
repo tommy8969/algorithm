05525d1e65569aa797905d87af99624ebd32d33a
tommy8969 <tommy8969@naver.com>
2025-02-19 17:24:50 +0900

    (baekjoon) 조건문 수정

--- baekjoon/src/main/java/BOJ_2525.java
+++ baekjoon/src/main/java/BOJ_2525.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BOJ_2525 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        int cookTime = sc.nextInt();
+
+        int endTime = 0;
+
+        if (M + cookTime >= 60) {
+           endTime = (M + cookTime) % 60;
+           H = H + ((M + cookTime) / 60);
+        } else {
+            endTime = M + cookTime;
+        }
+
+        if (H >= 24) {
+            H -= 24;
+        }
+
+        System.out.println(H + " " + endTime);
+
+    }
 }
