18349b11764be770d7dbd808e5798753d430c1c7
tommy8969 <tommy8969@naver.com>
2025-02-19 11:23:45 +0900

    BOJ 수정

---
+++ baekjoon/src/main/java/BOJ_10171.java
@@ -0,0 +1,9 @@
+public class Ex10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
---
+++ baekjoon/src/main/java/BOJ_10172.java
@@ -0,0 +1,5 @@
+public class Ex10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    }
+}
