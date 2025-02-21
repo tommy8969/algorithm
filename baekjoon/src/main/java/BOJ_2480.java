2ca2ba0f6531c56f9664133e9d5bc3e8e96acbdb
tommy8969 <tommy8969@naver.com>
2025-02-21 17:39:00 +0900

    (baekjoon) 조건문

--- baekjoon/src/main/java/BOJ_2480.java
+++ baekjoon/src/main/java/BOJ_2480.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BOJ_2480 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int fisrtNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+        int thirdNum = sc.nextInt();
+        int prize = 0;
+
+        if(fisrtNum == secondNum && fisrtNum == thirdNum) {
+            prize = 10000 + (fisrtNum * 1000);
+        } else if(fisrtNum == secondNum || fisrtNum == thirdNum) {
+            prize = 1000 + (fisrtNum * 100);
+        } else if(secondNum == thirdNum) {
+            prize = 1000 + (secondNum * 100);
+        } else {
+            prize = Math.max(fisrtNum, Math.max(secondNum, thirdNum)) * 100;
+        }
+
+        System.out.println(prize);
+
+    }
 }
