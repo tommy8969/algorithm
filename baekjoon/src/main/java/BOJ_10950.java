9d1ebd2d02fad3da766b2c58aafb1fad9f0fd7c6
tommy8969 <tommy8969@naver.com>
2025-02-24 21:14:44 +0900

    (baekjoon) 수정

---
+++ baekjoon/src/main/java/BOJ_10950.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+
+    }
+}
